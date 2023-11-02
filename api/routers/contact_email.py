ef90e85cb71998f12d1e47eda5da6d84920807e6
RyanWri <ranman37@gmail.com>
2023-11-02 10:23:35 +0200

    fastapi validate and send email

---
+++ api/routers/contact_email.py
@@ -0,0 +1,32 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import os
+
+
+def send_contact_form_to_my_email(sender_email, subject, message):
+    # Create a multipart message
+    msg = MIMEMultipart()
+    to = os.environ.get("SMTP_USERNAME")
+    msg["From"] = sender_email
+    msg["To"] = to
+    msg["Subject"] = subject
+
+    # Add the message body
+    msg.attach(MIMEText(message, "plain"))
+
+    # Connect to the SMTP server
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        # Start TLS for security
+        server.starttls()
+
+        # Enable debug mode
+        server.set_debuglevel(True)
+        
+        # Login to your Gmail account
+        server.login(to, os.environ.get('SMTP_APP_PASSWORD'))
+
+        # Send the email
+        server.send_message(msg)
+
+    print("Email sent successfully!")
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
+from email_validator import validate_email, EmailNotValidError
+from api.models.contact_email import ContactForm
+from api.routers.contact_email import send_contact_form_to_my_email
+
 
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
 
 @app.get("/predictions")
 def get_predictions():
@@ -16,3 +22,23 @@ def get_predictions():
 def create_prediction():
     # Logic to create a new prediction
     return {"message": "This is the POST predictions endpoint"}
+
+
+@app.post("/send-email")
+async def send_email(contact_form: ContactForm):
+    try:
+        # Check that the email address is valid.
+        emailinfo = validate_email(contact_form.from_address, check_deliverability=True)
+        # After this point, use only the normalized form of the email address
+        email = emailinfo.normalized
+        send_contact_form_to_my_email(email, "test email", contact_form.message)
+        return {"message": "Email sent successfully"}
+
+    except EmailNotValidError as email_err:
+        # The exception message is human-readable explanation of why it's not a valid (or deliverable) email address.
+        print(str(email_err))
+        return {"message": "Invalid email address"}
+    
+    except Exception as e:
+        print(e)
+        return {"message": "email delivery failed"}
